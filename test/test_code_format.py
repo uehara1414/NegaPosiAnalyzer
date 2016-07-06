850f82fdf48b77eee8f32647995506e7a8078c98
uehara1414 <b1015062@fun.ac.jp>
2016-07-06 20:14:50 +0900

    Add setting files

---
+++ test/test_code_format.py
@@ -0,0 +1,21 @@
+import unittest
+from nose.tools import ok_, eq_
+import pycodestyle
+import os
+
+
+class TestCodeFormat(unittest.TestCase):
+
+    def test_conformance(self):
+        """Test that we conform to PEP-8."""
+        filelist = []
+        for (root, dirs, files) in os.walk("."):
+            for name in files:
+                if name.endswith(".py"):
+                    filelist.append(os.path.join(root, name))
+
+        style = pycodestyle.StyleGuide(quiet=False)
+
+        result = style.check_files(filelist)
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
